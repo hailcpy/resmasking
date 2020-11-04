1ea8d4696d5b22e231f89a998bb9cd9c4c8425d0
Aditya Jindal <jaditya8889@gmail.com>
2020-11-04 20:06:37 +0530

    changed to get 12,36

--- models/resmasking.py
+++ models/resmasking.py
@@ -33,7 +33,7 @@ class ResMasking(ResNet):
         # state_dict = load_state_dict_from_url(model_urls['resnet34'], progress=True)
         # self.load_state_dict(state_dict)
  
-        self.fc = nn.Linear(512, 7)
+        # self.fc = nn.Linear(256, 7)
 
         '''
         # freeze all net
@@ -44,28 +44,22 @@ class ResMasking(ResNet):
         self.mask1 = masking(64, 64, depth=4)
         self.mask2 = masking(128, 128, depth=3)
         self.mask3 = masking(256, 256, depth=2)
-        self.mask4 = masking(512, 512, depth=1)
+        # self.mask4 = masking(512, 512, depth=1)
 
     def forward(self, x):  # 224 
         x = self.conv1(x)  # 112
-        print(x.shape)
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)  # 56
-        print(x.shape)
 
         x = self.layer1(x)  # 56 
-        print(x.shape)
         m = self.mask1(x)
         x = x * (1 + m)
-        print(x.shape)
         # x = x * m
 
         x = self.layer2(x)  # 28
-        print(x.shape)
         m = self.mask2(x)
         x = x * (1 + m)
-        print(x.shape)
         # x = x * m
 
         x = self.layer3(x)  # 14
@@ -73,17 +67,16 @@ class ResMasking(ResNet):
         x = x * (1 + m)
         # x = x * m
 
-        x = self.layer4(x)  # 7
-        print(x.shape)
-        m = self.mask4(x)
-        x = x * (1 + m)
-        print(x.shape)
-        # x = x * m
+        # x = self.layer4(x)  # 7
+        # m = self.mask4(x)
+        # x = x * (1 + m)
+        # # x = x * m
 
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
+        # x = self.avgpool(x)
+        # x = torch.flatten(x, 1)
+    
 
-        x = self.fc(x)
+        # x = self.fc(x)
         return x 
 
 
@@ -159,11 +152,11 @@ def resmasking50_dropout1(in_channels, num_classes, weight_path=''):
 
 def resmasking_dropout1(in_channels=1, num_classes=7, weight_path=''):
     model = ResMasking(weight_path)
-    model.fc = nn.Sequential(
-        nn.Dropout(0.4),
-        nn.Linear(512, 7)
-        # nn.Linear(512, num_classes)
-    )
+    # model.fc = nn.Sequential(
+    #     # nn.Dropout(0.4),
+    #     # nn.Linear(256, 7)
+    #     # nn.Linear(512, num_classes)
+    # )
     return model
 
 
--- models/resnet.py
+++ models/resnet.py
@@ -142,16 +142,16 @@ class ResNet(nn.Module):
 
         # NOTE: strictly set the in_channels = 3 to load the pretrained model
         # self.conv1 = nn.Conv2d(1, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
-        self.conv1 = nn.Conv2d(1, self.inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(1, self.inplanes, kernel_size=5, stride=1, padding=2, bias=False)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=1,
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
                                        dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=1,
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
                                        dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=1,
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
